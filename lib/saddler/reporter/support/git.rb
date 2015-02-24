b035c6f9e0403d0b3cef5a09b109f7ab65b3debe
sanemat <o.gata.ken@gmail.com>
2015-02-24 21:34:35 +0900

    Initial commit

---
+++ lib/saddler/reporter/support/git.rb
@@ -0,0 +1,11 @@
+require "saddler/reporter/support/git/version"
+
+module Saddler
+  module Reporter
+    module Support
+      module Git
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/saddler/reporter/support/git/version.rb
@@ -0,0 +1,9 @@
+module Saddler
+  module Reporter
+    module Support
+      module Git
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
