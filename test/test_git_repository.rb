e2ece1ef130447984c007d743395996b3a16808b
sanemat <o.gata.ken@gmail.com>
2015-02-25 10:48:00 +0900

    chore(test): setup repository

--- test/test_git_repository.rb
+++ test/test_git_repository.rb
@@ -5,6 +5,14 @@ module Saddler
     module Support
       module Git
         class TestGitRepository < Test::Unit::TestCase
+          def setup
+            @repository = Repository.new(
+              REPO_PATH,
+              repository: File.join(REPO_PATH, 'testrepo.git'),
+              index: File.join(REPO_PATH, 'testrepo.git', 'index')
+            )
+          end
+
           test 'version' do
             assert do
               !::Saddler::Reporter::Support::Git::VERSION.nil?
