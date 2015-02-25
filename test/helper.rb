b313f163a19bd6615d364212a47ddf344c1177dd
sanemat <o.gata.ken@gmail.com>
2015-02-25 11:58:08 +0900

    chore(gem): use test-unit-rr

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'test/unit'
+require 'test/unit/rr'
 require 'saddler/reporter/support/git'
 REPO_PATH = File.expand_path('../fixtures', __FILE__)
