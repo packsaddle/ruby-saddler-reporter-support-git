0c033942a9dba23cafe70195f554f6ce65c042fa
sanemat <o.gata.ken@gmail.com>
2015-02-25 12:18:38 +0900

    chore(gem): use mocha instead of rr
    rr '12/27/14: This gem is no longer maintained.'

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'test/unit'
-require 'test/unit/rr'
+require 'mocha/test_unit'
 require 'saddler/reporter/support/git'
 REPO_PATH = File.expand_path('../fixtures', __FILE__)
