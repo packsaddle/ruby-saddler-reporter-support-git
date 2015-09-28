df598ac61bf87325bf64102b6d37323ed7391428
sanemat <o.gata.ken@gmail.com>
2015-09-28 21:29:20 +0900

    chore(test): use EnvBranch's test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'test/unit'
 require 'mocha/test_unit'
+require 'env_branch/test_helper'
 require 'saddler/reporter/support/git'
 REPO_PATH = File.expand_path('../fixtures', __FILE__)
