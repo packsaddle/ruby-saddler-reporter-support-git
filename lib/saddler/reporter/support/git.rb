15216b70ced5079352d1b44fa814129ded582250
Sanemat <o.gata.ken@gmail.com>
2016-05-14 17:27:11 +0900

    docs(app): add document

--- lib/saddler/reporter/support/git.rb
+++ lib/saddler/reporter/support/git.rb
@@ -4,8 +4,11 @@ require 'env_branch'
 require 'saddler/reporter/support/git/version'
 require 'saddler/reporter/support/git/repository'
 
+# saddler
 module Saddler
+  # saddler reporter
   module Reporter
+    # saddler reporter support
     module Support
       # Git support utility for saddler-reporter
       module Git
--- lib/saddler/reporter/support/git/version.rb
+++ lib/saddler/reporter/support/git/version.rb
@@ -2,6 +2,7 @@ module Saddler
   module Reporter
     module Support
       module Git
+        # version
         VERSION = '1.0.0'.freeze
       end
     end
